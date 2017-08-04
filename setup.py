5cf8e1114622e1c5bfe96b3fc0a5bbb344f6b2ec
Kimihiro N <kimihiro.nisimura@gmail.com>
2017-08-04 14:21:22 +0900

    Fixed package

--- setup.py
+++ setup.py
@@ -9,15 +9,16 @@ if sys.version_info < (3, 6, 1):
 
 setup(
     name='dj_database_waiter',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/pistatium/dj_database_waiter',
     author='pistatium',
+    author_email='kimihiro.nisimura@gmail.com',
     description='Script to keep waiting until Django DB is ready.',
-    packages=['.'],
+    packages=['dj_database_waiter'],
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'dj_database_waiter=cmd:main'
+            'dj_database_waiter=dj_database_waiter.cmd:main'
         ]
     },
 )
